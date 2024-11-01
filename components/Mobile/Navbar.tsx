c08e168adab5a64e347a6438a6b6824c37a947f5
NicholasGoh <gohn0004@e.ntu.edu.sg>
2024-11-01 21:39:26 +0800

    remove login

--- components/Mobile/Navbar.tsx
+++ components/Mobile/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Conversation } from '@/types/chat';
 import { IconPlus } from '@tabler/icons-react';
 import { FC } from 'react';
-import { UserButton } from '@clerk/clerk-react';
 
 interface Props {
   selectedConversation: Conversation;
@@ -13,22 +12,17 @@ export const Navbar: FC<Props> = ({
   onNewConversation,
 }) => {
   return (
-    <>
-      <div style={{ background: 'red' }}>
-        <UserButton />
+    <nav className="flex w-full justify-between bg-[#202123] py-3 px-4">
+      <div className="mr-4"></div>
+
+      <div className="max-w-[240px] overflow-hidden text-ellipsis whitespace-nowrap">
+        {selectedConversation.name}
       </div>
-      <nav className="flex w-full justify-between bg-[#202123] py-3 px-4">
-        <div className="mr-4"></div>
 
-        <div className="max-w-[240px] overflow-hidden text-ellipsis whitespace-nowrap">
-          {selectedConversation.name}
-        </div>
-
-        <IconPlus
-          className="mr-8 cursor-pointer hover:text-neutral-400"
-          onClick={onNewConversation}
-        />
-      </nav>
-    </>
+      <IconPlus
+        className="mr-8 cursor-pointer hover:text-neutral-400"
+        onClick={onNewConversation}
+      />
+    </nav>
   );
 };
