9f37a41ce58274854c9cd161f1b145849d1824db
NicholasGoh <gohn0004@e.ntu.edu.sg>
2025-03-29 01:31:35 +0800

    feat(google analytics): add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
 import { ClerkProvider } from '@clerk/clerk-react';
+('use client');
+
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY || '';
@@ -12,11 +15,60 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key');
 }
 
+const GA_TRACKING_ID = 'G-TECW05ZKH4';
+
+const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+interface GTagEvent {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+}
+
+const event = ({ action, category, label, value }: GTagEvent) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                      });
+                    `,
+        }}
+      />
+    </>
+  );
+};
+
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <div className={inter.className}>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/ui">
         <Toaster position="top-right" />
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </ClerkProvider>
     </div>
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 export default defineConfig({
   resolve: {
@@ -7,4 +8,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './'),
     },
   },
+  plugins: [
+    VitePluginRadar({
+      // Google Analytics tag injection
+      // TODO: https://github.com/stafyniaksacha/vite-plugin-radar/blob/v0.10.0/README.md?plain=1#L90
+      // might need custom events for more analysis
+      analytics: {
+        id: 'G-TECW05ZKH4',
+      },
+    }),
+  ],
 });
